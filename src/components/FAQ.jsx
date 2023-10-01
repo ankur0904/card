ddaf77a4fe2682cc3acdd9bb1ea0e9335a0ed0ef
Sainadh Annavarapu <annavarapusainadh2002@gmail.com>
2023-10-01 12:38:05 +0530

    newcommit

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function FAQ(){
     return (
         <>
-        
+        Just a custom FAQ message.
         </>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Card from "../components/Card";
 import NavBar from "../components/NavBar";
+import FAQ from "../components/FAQ";
 
 function HomePage() {
     return (
         <>
             <NavBar />
             <Card />
+            <FAQ />
         </>
 
     )
