469e8decb0c00acb958f4792658d43980d13ec5e
Anubrat Sahoo <anubrat23@gmail.com>
2023-10-01 12:17:33 +0530

    completed the AboutUsPage.jsx file

---
+++ src/pages/AboutUsPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function ProjectDescription() {
+  return (
+    <div>
+      <p>
+        This project offers invaluable guidance and hands-on experience for beginners looking to step into the world of open-source development. By breaking down the process of making a pull request into easy-to-follow steps, it demystifies the often daunting world of contributing to open-source projects. It welcomes individuals with various levels of technical expertise, even those with minimal IT backgrounds, providing a welcoming and inclusive environment for learning and growth. Through this project, beginners can gain practical experience in forking repositories, making code changes, and creating pull requests, skills that are fundamental for anyone aspiring to be a web or app developer. Furthermore, the project's emphasis on clear instructions and a supportive community makes it an ideal starting point for individuals seeking to build confidence and build connections within the tech community, aligning perfectly with their aspirations to foster a large tech community of their own.
+      </p>
+      <p>
+        Connect with the author:
+      </p>
+      <ul>
+        <li>
+          <a href="https://twitter.com/authorTwitter">Twitter</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/authorLinkedIn">LinkedIn</a>
+        </li>
+        <li>
+          Email: <a href="mailto:authorEmail@gmail.com">authorEmail@gmail.com</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default ProjectDescription;
